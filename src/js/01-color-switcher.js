0461488c6b8f675ca5ff283b843b0d06e9511944
Mariia1811 <115475304+Mariia1811@users.noreply.github.com>
2023-01-04 14:48:48 +0200

    add completed task one

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  bodyEl: document.querySelector('body'),
+};
 
+let timerId = null;
+refs.stopBtn.disabled = true;
+
+refs.startBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = true;
+  refs.stopBtn.disabled = false;
+
+  timerId = setInterval(() => {
+    const colorRandom = getRandomHexColor();
+    refs.bodyEl.style.backgroundColor = `${colorRandom}`;
+  }, 1000);
+});
+
+refs.stopBtn.addEventListener('click', () => {
+  refs.startBtn.disabled = false;
+  refs.stopBtn.disabled = true;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
